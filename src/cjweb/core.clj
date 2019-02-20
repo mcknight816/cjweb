f5910c8f330c7217c3da958d188386b696ea34fe
mcknight816 <mcknight816@gmail.com>
2019-02-20 16:33:23 -0500

    Allow for a trailing slash in the endpoint url, make server an atom so we can shut it down

--- src/cjweb/core.clj
+++ src/cjweb/core.clj
@@ -1,18 +1,36 @@
 (ns cjweb.core (:require [org.httpkit.server :as s]
                          [clojure.tools.logging :as log]
                          [cjweb.mongo.api :as mongo_api]))
-
-(defn app "Attach API routes to our web application" []
-    (mongo_api/mongo_routes))
 ;;todo allow for external configuration of the port
 ;;todo add security
+(defonce server (atom nil))
+
+(defn ignore-trailing-slash
+  "Modifies the request uri before calling the handler.
+  Removes a single trailing slash from the end of the uri if present."
+  [handler]
+  (fn [request]
+    (let [uri (:uri request)]
+      (handler (assoc request :uri (if (and (not (= "/" uri))
+                                            (.endsWith uri "/"))
+                                     (subs uri 0 (dec (count uri)))
+                                     uri))))))
+
+(defn app "Attach API routes to our web application" []
+    (ignore-trailing-slash  (mongo_api/mongo_routes )))
+
 (defn create-server "Start the web server" []
-  (s/run-server (app) {:port 8080})
+  (reset! server (s/run-server (#'app) {:port 8080}))
   (log/info (str "Create a server on port 8080 " "http://localhost:8080/mongo")))
 
-(defn stop-server "Stop the web server" [server]
-  (server :timeout 100))
+(defn stop-server []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)))
 
 (defn -main "Start the app" []
   (create-server ))
+
+(comment (create-server))
+(comment (stop-server))
 
