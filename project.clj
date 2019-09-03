d99544a633e4486283b2337404a73e1e407b704c
amcknight <amcknight@relaynetwork.com>
2019-09-03 08:30:46 -0400

    change git ignore to add iml files

--- project.clj
+++ project.clj
@@ -4,8 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [eureka-client "0.2.0"]
                  [http-kit "2.3.0"]
                  [compojure "1.6.0"]
                  [cheshire "5.8.1"]
                  [org.clojure/tools.logging "0.4.1"]
-                 [com.novemberain/monger "3.5.0"]])
+                 [com.novemberain/monger "3.5.0"]
+                 [com.unbounce/clojure-dogstatsd-client "0.6.0"]
+                 [datadog-apm-clj-wrapper "0.1.2"]; used to test data dog apm
+                 ])
